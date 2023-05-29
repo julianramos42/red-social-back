c7ca2516e51af5bef1aae78d49287ae3f8041652
Julian <julianete4@gmail.com>
2023-05-29 19:06:41 -0300

    correccion al crear conexion

--- controllers/conections/create.js
+++ controllers/conections/create.js
@@ -2,6 +2,7 @@ import Conection from '../../models/Conection.js'
 
 export default async function create(req,res,next){
     try{
+        req.body.user_id2 = req.user.id
         await Conection.create(req.body)
         return res.status(201).json({
             success: true,
