cd529f17b0fca2274379820d2057571b64aef8a6
Julian <julianete4@gmail.com>
2023-05-31 12:59:50 -0300

    arreglo

--- bin/www.js
+++ bin/www.js
@@ -92,7 +92,7 @@ function onListening() {
 
 const io = new Server(server, {
   cors: {
-      origin: 'https://jr-red-social.vercel.app' || 'http://localhost:3000',
+      origin: 'https://jr-red-social.vercel.app',
       methods: ['GET', 'POST']
   }
 });
